c96d836993a7496a7855d623d05274c410a7459e
Fauz Ahmed <fauzahmed2@gmail.com>
2024-06-13 05:07:13 -0700

    edit projects

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -2,23 +2,39 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"HealthHub",
-        s_desc:"A complete medical web portal with integrated AI medical assistance chatbot."
+        s_desc:". A complete medical web portal with integrated AI medical assistance chatbot.",
+        s_tools:". Technologies used: HTML/CSS, JavaScript, Node.js and Gemini API"
     },
     {
         s_no:"02",
-        s_name:"TimeTable Scheduling",
-        s_desc:"Implemented Genetic Algorithm to schedule a clash-free timetable"
+        s_name:"Maidaan",
+        s_desc:"Football Matchmaking platform",
+        s_tools:". Technologies used: JavaFx, Scene Builder, MySQL, Oracle, Microsoft SQL Server"
     },
     {
         s_no:"03",
-        s_name:"Maidaan",
-        s_desc:"Football Matchmaking platform"
+        s_name:"TimeTable Scheduling",
+        s_desc:"Implemented Genetic Algorithm to schedule a clash-free timetable",
+        s_tools:". Technologies used: Python"
     },
     {
         s_no:"04",
-        s_name:"Neural Network",
-        s_desc:"Operating system depicting Nueral-Network architecture on a multicore processor"
+        s_name:"Personal Portfolio Wesbite",
+        s_desc:"Developed a fully functional user friendly Portfolio website",
+        s_tools:". Technologies used: React.js, Git"
     },
+    {
+        s_no:"05",
+        s_name:"Neural Network",
+        s_desc:"Operating system depicting Nueral-Network architecture on a multicore processor",
+        s_tools:". Technologies used: C++, Linux"
+    },
+    {
+        s_no:"06",
+        s_name:"Kth",
+        s_desc:"Operating system depicting Nueral-Network architecture on a multicore processor",
+        s_tools:". Technologies used: C++, Linux"
+    }
 ]
  
 export default Services_Data;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -18,7 +18,8 @@ const Projects = () => {
                 <h3>01</h3>
                 <h2>HealthHub</h2>
                 <p>
-                    A complete medical web portal with integrated AI medical assistance chatbot.
+                    - A complete medical web portal with integrated AI medical assistance chatbot.<br/>
+                    - Technologies used: HTML/CSS, JavaScript, Node.js and Gemini API
                 </p>
                 <div className="projects-readmore">
                      <p>Source Code</p>
@@ -31,7 +32,22 @@ const Projects = () => {
                 <h3>02</h3>
                     <h2>Maidaan</h2>
                     <p>
-                        A complete Full stack Football Matchmaking platform.
+                        - A complete Full stack Football Matchmaking platform.<br/>
+                        - Technologies used: JavaFx, Scene Builder, MySQL, Oracle, Microsoft SQL Server
+                    </p>
+                    <div className="projects-readmore">
+                         <p>Source Code</p>
+                        <img src={arrow_icon} />
+                    </div>
+            </div>
+            </a>
+            <a href='https://github.com/fauz2003/Fauz-Portfolio' target='blank'>
+            <div className="projects-format">
+                <h3>03</h3>
+                    <h2>Personal Portfolio Website</h2>
+                    <p>
+                    - Developed a fully functional user friendly Portfolio website<br/>
+                    - Technologies used: React.js, Git
                     </p>
                     <div className="projects-readmore">
                          <p>Source Code</p>
@@ -41,10 +57,12 @@ const Projects = () => {
             </a>
             <a href='https://github.com/fauz2003/TimeTable-Scheduling' target='blank'>
             <div className="projects-format">
-                <h3>03</h3>
+                <h3>04</h3>
                     <h2>TimeTable Scheduling</h2>
                     <p>
-                    Implemented Genetic Algorithm to schedule a clash-free timetable.
+                    - Implemented Genetic Algorithm to schedule a clash-free timetable.
+                    <br/>
+                    - Technologies used: Python
                     </p>
                     <div className="projects-readmore">
                          <p>Source Code</p>
@@ -54,10 +72,11 @@ const Projects = () => {
             </a>
             <a href='https://github.com/fauz2003/Neural-Network' target='blank'>
                 <div className="projects-format">
-                    <h3>04</h3>
+                    <h3>05</h3>
                     <h2>Neural Network</h2>
                     <p>
-                        Operating system depicting Nueral-Network architecture on a multicore processor.
+                        - Operating system depicting Nueral-Network architecture on a multicore processor.<br/>
+                        - Technologies used: C++, Linux
                     </p>
                     <div className="projects-readmore">
                          <p>Source Code</p>
@@ -67,10 +86,11 @@ const Projects = () => {
             </a>
             <a href='https://github.com/fauz2003/Kth-Shortest-Path-Problem' target='blank'>
                 <div className="projects-format">
-                    <h3>05</h3>
+                    <h3>06</h3>
                     <h2>Kth Shortest Path Problem</h2>
                     <p>
-                        Used MPI and OpenMP to parallelize the Kth Shortest Path Problem.
+                        - Used MPI and OpenMP to parallelize the Kth Shortest Path Problem.<br/>
+                        - Technologies used: C++, OpenMP, MPI
                     </p>
                     <div className="projects-readmore">
                          <p>Source Code</p>
